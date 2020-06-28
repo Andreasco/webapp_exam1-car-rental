4e18280a3de61140753cc999f523aa6d32e4cd09
Andrea Scoppetta <andre.sco@icloud.com>
2020-06-28 19:15:56 +0200

    Fixed some warnings

--- client/src/components/LoginNotice.js
+++ client/src/components/LoginNotice.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
-import {AuthContext} from '../auth/AuthContext'
 
 // Fontawesome imports
 import { faHandPointUp } from "@fortawesome/free-regular-svg-icons";
@@ -9,20 +8,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const LoginNotice = () => {
     return (
-        <AuthContext.Consumer>
-            {(context) => (
-                <>
-                    <Jumbotron fluid>
-                        <Container>
-                            <h1 style={{fontSize : "3rem"}}>
-                                Oops! It seams that you're not logged in yet. <FontAwesomeIcon icon={faHandPointUp} />
-                            </h1>
-                            <p style={{fontSize : "1.5rem"}}>Please login using the form in the upper right corner.</p>
-                        </Container>
-                    </Jumbotron>
-                </>
-            )}
-        </AuthContext.Consumer>
+        <>
+            <Jumbotron fluid>
+                <Container>
+                    <h1 style={{fontSize : "3rem"}}>
+                        Oops! It seams that you're not logged in yet. <FontAwesomeIcon icon={faHandPointUp} />
+                    </h1>
+                    <p style={{fontSize : "1.5rem"}}>Please login using the form in the upper right corner.</p>
+                </Container>
+            </Jumbotron>
+        </>
     );
 }
 
